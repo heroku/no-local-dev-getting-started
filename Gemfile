a46c774fd861f485c97ea72755eb31f73af7f4c6
Juan Zalles <juan.zalles@gmail.com>
2017-05-05 13:46:04 -0500

    first pass at cowsay routes

--- Gemfile
+++ Gemfile
@@ -1,11 +1,15 @@
 source 'https://rubygems.org'
-ruby "2.2.0"
+ruby "2.2.5"
 
-gem "sinatra" 
-gem "activerecord" 
+gem "sinatra"
+gem "activerecord"
 gem "sinatra-activerecord"
+gem "sinatra-contrib"
+gem 'shotgun'
 
 gem 'pg'
+gem 'pry'
+gem 'cowsay'
 
 # a little console
 gem "tux"
--- app.rb
+++ app.rb
@@ -1,14 +1,39 @@
 # app.rb
 
 require 'sinatra'
+require 'sinatra/reloader'
 require 'sinatra/activerecord'
 require './environments'
+require 'cowsay'
 
 
 get "/" do
   erb :home
 end
 
+get "/cowsay" do
+  @character = "Cow"
+  @message = "Hello"
+  erb :'/cowsay/_cowsay'
+
+  # '<pre>' + Cowsay.say("Hello", "Cow") +'</pre>'
+end
+
+get "/cowsay/all" do
+  response = ""
+  Cowsay.character_classes.each do |who|
+    response += "<h4> #{who.to_s} </h4><hr>"
+      response += '<pre>' + Cowsay.say("Hello", who.to_s) +'</pre>'
+    end
+    puts response
+    response
+end
+get "/cowsay/random/:string" do
+  '<pre>' + Cowsay.random_character.say(params[:string]) + '</pre>'
+end
+get "/cowsay/:character/:string" do
+  '<pre>' + Cowsay.say(params[:string], params[:character]) +'</pre>'
+end
 
 class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
