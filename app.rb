22c3631afb7416b7b4e95f2a3c55ee0baab78d5c
Yasuhiro Miyake <ymiyake@ymiyake-ltm.internal.salesforce.com>
2019-03-18 13:46:47 +0800

    add contact list

--- app.rb
+++ app.rb
@@ -14,14 +14,15 @@ class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
 end
 
-#get "/contacts" do
-#  @contacts = Contact.all
-#  erb :index
-#end
 
 get "/create" do
   dashboard_url = 'https://dashboard.heroku.com/'
   match = /(.*?)\.herokuapp\.com/.match(request.host)
   dashboard_url << "apps/#{match[1]}/resources" if match && match[1]
   redirect to(dashboard_url)
+end
+
+get "/contacts" do
+  @contacts = Contact.all
+  erb :index
 end
