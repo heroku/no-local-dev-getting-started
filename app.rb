a7653214f62a7dd5c526c9105d6d2564188eca2c
deeptiwari <deeptiwariwork@gmail.com>
2020-05-20 22:32:37 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ get "/" do
 end
 
 class Contact < ActiveRecord::Base
- self.table_name = 'salesforce.contact'
+ self.table_name = 'salesforce.account'
 end
 # Start - Added by Deep
 #class Account < ActiveRecord::Base
