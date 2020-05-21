333e7749e2a7ee6230429e561e22eaae2cc3d40f
deeptiwari <deeptiwariwork@gmail.com>
2020-05-20 21:44:18 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -18,6 +18,10 @@ end
 #  @contacts = Contact.all
 #  erb :index
 #end
+get "/contacts" do
+  @contacts = Contact.all
+  erb :index
+end
 
 get "/create" do
   dashboard_url = 'https://dashboard.heroku.com/'
