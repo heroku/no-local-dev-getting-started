42984a000a9581149599eafb030eda8efd7bc367
Julian Duque <julianduquej@gmail.com>
2024-02-08 15:36:33 -0500

    fix: remove subdomain identifier

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ end
 
 get "/create" do
   dashboard_url = 'https://dashboard.heroku.com/'
-  match = /(.*?)\.herokuapp\.com/.match(request.host)
+  match = /(.*?)(-\w+)?\.herokuapp\.com/.match(request.host)
   dashboard_url << "apps/#{match[1]}/resources" if match && match[1]
   redirect to(dashboard_url)
 end
