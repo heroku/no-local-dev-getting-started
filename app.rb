083f4b80f18be68716428e0d471e2ffcb2bfc368
Julian Duque <julianduquej@gmail.com>
2024-02-09 11:49:25 -0500

    feat: uncomment contacts route

--- app.rb
+++ app.rb
@@ -9,15 +9,14 @@ get "/" do
   erb :home
 end
 
-
 class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
 end
 
-#get "/contacts" do
-#  @contacts = Contact.all
-#  erb :index
-#end
+get "/contacts" do
+ @contacts = Contact.all
+ erb :index
+end
 
 get "/create" do
   dashboard_url = 'https://dashboard.heroku.com/'
